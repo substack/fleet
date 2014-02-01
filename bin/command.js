5083a7677e6c8312439447c741181d062982acd9
Joooom <jonma752@student.liu.se>
2014-02-01 14:32:26 +0100

    removed comments

--- bin/command.js
+++ bin/command.js
@@ -29,18 +29,3 @@ if (process.argv.length < 3) {
 	}
 
 }
-
-/*
-if test -z "$*"; then
-  $dirname/fleet-help
-elif test "$*" = '-v' || test "$*" = '--version'; then
-  $dirname/fleet-version
-elif test -f "$dirname/fleet-$1"; then
-  $dirname/fleet-$*
-else
-  echo "Fleet command \"$1\" not recognized."
-  echo 'Type `fleet help` to see a list of all commands.'
-fi
-*/
-
-
