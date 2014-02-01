355cc02ea58c3b0f76087d7c3fecbe03b93ebfde
Joooom <jonma752@student.liu.se>
2014-02-01 13:32:08 +0100

    fixed up help

--- bin/help.js
+++ bin/help.js
@@ -5,8 +5,8 @@ var fs = require('fs'),
 if (process.argv.length < 3) {
 	var help = fs.readFileSync(__dirname + '/../doc/commands', 'utf8')
 	console.log(help)
-} else if (fs.existsSync(__dirname + '/../man/' + process.argv[2] + '.1')) {
-	var man = spawn('man', [__dirname + '/../man/' + process.argv[2] + '.1'])
+} else if (fs.existsSync(__dirname + '/../man1/' + process.argv[2] + '.1')) {
+	var man = spawn('man', [__dirname + '/../man1/' + process.argv[2] + '.1'])
 	man.stdout.pipe(process.stdout)
 	man.stderr.pipe(process.stderr)
 } else {
